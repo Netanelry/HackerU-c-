e8d4af80b795493267bddeff678a8eb213fa7894
Netanelry <66564358+Netanelry@users.noreply.github.com>
2020-07-15 05:52:12 +0300

    Add files via upload

---
+++ homwork 12-07-2020/Citizen.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Citizen
+    {
+        public string _name;
+        public Citizen[] _children;
+        public readonly int _fatherID;
+        public readonly int _id;
+        public static int _numOfCurrentCitizens = 0;
+        public const int _MAX_NUM_OF_CITIZENS = 10;
+
+        public Citizen(string name, int fatherID)
+        {
+            Citizen._numOfCurrentCitizens++;
+            _id = Citizen._numOfCurrentCitizens;
+            _name = name;
+            _fatherID = fatherID;
+        }
+        public static void PrintNumOfCitizens()
+        {
+            Console.WriteLine (_numOfCurrentCitizens);
+        }
+
+        public static bool IsMaxSizeReached()
+        {
+            bool isMax;
+            if (_numOfCurrentCitizens <= _MAX_NUM_OF_CITIZENS)
+                isMax = true;
+            else
+                isMax = false;
+            return isMax;
+        }
+
+        public void PrintID()
+        {
+            Console.WriteLine(_id);
+        }
+
+        public void PrintGapBetweenMyIDAndFather()
+        {
+            Console.WriteLine(_id - _fatherID);
+        }
+
+        public void SetChildren(Citizen[] Childrens)
+        {
+            _children = Childrens;
+        }
+
+        public override string ToString()
+        {
+            return $"CITIZEN INFO.\nName: {_name}\nID: {_id}\nFather ID: {_fatherID}\nKnown Childrens: {_children}";
+        }
+    }
+}
---
+++ homwork 12-07-2020/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Citizen c1 = new Citizen("Kane", 0);
+            Citizen c2 = new Citizen("kanny", 1);
+            Citizen c3 = new Citizen("Kane jr", 1);
+            Citizen c4 = new Citizen("fridrick augustus the 3rd", 1);
+
+            Citizen[] childrens = new Citizen[]
+            {
+                    c2,
+                    c3,
+                    c4,
+            };
+
+            c1.SetChildren(childrens);
+        }
+
+        public bool HasChildren(Citizen citizen)
+        {
+            return citizen._children.Length != 0;
+        }
+
+        public bool ChackValidity(Citizen citizen)
+        {
+            bool chack = false;
+            if (HasChildren(citizen))
+            {
+                foreach (Citizen children in citizen._children)
+                {
+                    if (children._fatherID == citizen._id)
+                        chack = true;
+                }
+            }
+            return chack;
+        }
+    }
+}
